021f763f50e39319b2c45df7ea534e6fcffa0398
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2021-10-26 18:44:53 +0300

    add update link, refactor

--- reddit-post-unhider.user.js
+++ reddit-post-unhider.user.js
@@ -3,19 +3,19 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://old.reddit.com/user/*/hidden/
 // @grant       none
-// @version     1.0
-// @description Go to old reddit -> hidden posts
-//              The script will unhide all posts on the current
-//              page then reload to get new ones
+// @version     1.0.1
+// @updateURL   https://github.com/danielvigaru/reddit-post-unhider.user.js/raw/main/reddit-post-unhider.user.js
+// @downloadURL https://github.com/danielvigaru/reddit-post-unhider.user.js/raw/main/reddit-post-unhider.user.js
+// @description Go to old reddit -> hidden posts. The script will unhide all posts on the current page then reload to get new ones
 // ==/UserScript==
 
 window.onload = () => {
-  let flag = false;
+  let pageDone = false;
   setInterval(() => {
     try {
-      if (!flag) $('.unhide-button a')[0].click();
+      if (!pageDone) $('.unhide-button a')[0].click();
     } catch {
-      flag = true;
+      pageDone = true;
       location.reload();
     }
   }, 750);
