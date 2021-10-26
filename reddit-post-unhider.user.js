fc1ee4d7d0467ebfe457db14effde62cc9747f33
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2021-10-26 18:47:29 +0300

    add homepage

--- reddit-post-unhider.user.js
+++ reddit-post-unhider.user.js
@@ -6,6 +6,7 @@
 // @version     1.0.1
 // @updateURL   https://github.com/danielvigaru/reddit-post-unhider.user.js/raw/main/reddit-post-unhider.user.js
 // @downloadURL https://github.com/danielvigaru/reddit-post-unhider.user.js/raw/main/reddit-post-unhider.user.js
+// @homepageURL https://github.com/danielvigaru/reddit-post-unhider.user.js
 // @description Go to old reddit -> hidden posts. The script will unhide all posts on the current page then reload to get new ones
 // ==/UserScript==
 
