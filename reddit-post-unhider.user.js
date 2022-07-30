4406bacb47a7cd251a3b56021f4b53ba2b747cff
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2022-07-30 23:46:51 +0300

    rewrite as self invoking function

--- reddit-post-unhider.user.js
+++ reddit-post-unhider.user.js
@@ -3,23 +3,19 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://old.reddit.com/user/*/hidden/
 // @grant       none
-// @version     1.0.2
+// @version     1.0.3
 // @updateURL   https://github.com/danielvigaru/reddit-post-unhider.user.js/raw/main/reddit-post-unhider.user.js
 // @downloadURL https://github.com/danielvigaru/reddit-post-unhider.user.js/raw/main/reddit-post-unhider.user.js
 // @homepageURL https://github.com/danielvigaru/reddit-post-unhider.user.js
 // @description Go to old reddit -> hidden posts. The script will unhide all posts on the current page then reload to get new ones
 // ==/UserScript==
 
-window.onload = () => {
-  let pageDone = false;
-  setInterval(() => {
-    if (!pageDone) {
-      try {
-        $('.unhide-button a')[0].click();
-      } catch {
-        pageDone = true;
-        location.reload();
-      }
-    }
-  }, 750);
-};
+(function () {
+    setInterval(() => {
+        try {
+            $(".unhide-button a")[0].click();
+        } catch {
+            location.reload();
+        }
+    }, 1000);
+})();
